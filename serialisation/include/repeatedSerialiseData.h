ac9768732cf71a4f03c876fd13558d05acd17b1c
Paul Visscher <paul.pev@hotmail.com>
2014-02-17 12:54:50 +0100

    Improved iterators

--- serialisation/include/repeatedSerialiseData.h
+++ serialisation/include/repeatedSerialiseData.h
@@ -23,7 +23,7 @@ public:
 
     virtual void WriteToStream( std::ostream &stream ) const
     {
-        for ( std::vector< DataType >::const_iterator it = mFields.begin(), end = mFields.end(); it != end; ++it )
+        for ( Fields::const_iterator it = mFields.begin(), end = mFields.end(); it != end; ++it )
         {
             it->WriteToStream( stream );
         }
@@ -31,7 +31,7 @@ public:
 
     virtual void ReadFromStream( std::istream &stream )
     {
-        for ( std::vector< DataType >::iterator it = mFields.begin(), end = mFields.end(); it != end; ++it )
+        for ( Fields::iterator it = mFields.begin(), end = mFields.end(); it != end; ++it )
         {
             it->ReadFromStream( stream );
         }
@@ -67,7 +67,9 @@ public:
 
 protected:
 
-    std::vector< DataType > mFields;
+    typedef std::vector< DataType > Fields;
+
+    Fields mFields;
 };
 
 typedef RepeatedData< Message, Type::Message > RepeatedMessage;
