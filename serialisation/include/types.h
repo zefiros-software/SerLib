a0cc35020700cefcc64e7ef907a25fffa64127f7
Paul Visscher <paul.pev@hotmail.com>
2014-02-17 15:42:16 +0100

    Removed dependency

--- serialisation/include/types.h
+++ serialisation/include/types.h
@@ -2,23 +2,7 @@
 #ifndef __TYPES_H__
 #define __TYPES_H__
 
-#include "macro.h"
-
-#ifdef CPP11
-#   include <stdint.h>
-#else
-
-typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
-typedef unsigned int uint32_t;
-typedef unsigned long long uint64_t;
-
-typedef signed char int8_t;
-typedef short int16_t;
-typedef int int32_t;
-typedef long long int64_t;
-
-#endif
+#include <stdint.h>
 
 namespace Type
 {
@@ -38,8 +22,6 @@ namespace Type
 
 namespace Mode
 {
-
-
     enum Mode
     {
         Serialise = 0x00,
