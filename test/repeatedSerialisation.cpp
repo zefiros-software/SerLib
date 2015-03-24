18664c9dc8b1dfc5d5bf4f98eff482be673aa3aa
KoenVisscher <koen.visscher@hotmail.com>
2015-03-24 21:44:15 +0100

    New test with very long arrays ( mimics a real use case for storing data sets )

--- test/repeatedSerialisation.cpp
+++ test/repeatedSerialisation.cpp
@@ -29,6 +29,14 @@
 #include <stdio.h>
 #include <vector>
 
+#define TestMeshDataClass( test, name, seed1, seed2, its1, its2, its3, its4 ) \
+    TEST( P( test ), name )                                          \
+    {                                                                \
+        ZefirosEngineMeshData< its1, its2, its3, its4 > c1( seed1 ), c2( seed2 );            \
+        SimpleSerialiseDeserialiseStream( c1, c2 );                  \
+        c1.TestEqual( c2 );                                          \
+    }
+
 #define TestEasyRepeatedClass( test, name, seed1, seed2, its ) \
     TEST( P( test ), name )                                          \
     {                                                                \
@@ -55,6 +63,169 @@
 
 namespace TestClasses
 {
+    template< uint32_t its1, uint32_t its2, uint32_t its3, uint32_t its4 >
+    class ZefirosEngineMeshData
+        : public ISerialisable
+    {
+    public:
+
+        class Vec3
+        {
+        public:
+
+            Vec3()
+            {
+                mData[0] = 0.0;
+                mData[1] = 0.0;
+                mData[2] = 0.0;
+            }
+
+            Vec3( float x, float y, float z )
+            {
+                mData[0] = x;
+                mData[1] = y;
+                mData[2] = z;
+            }
+
+            void SERIALISATION_CUSTOM_INTERFACE( Message &message )
+            {
+                message.CreateArray( Type::Float, 3, 0 );
+                message.StoreArrayItem( mData[0] );
+                message.StoreArrayItem( mData[1] );
+                message.StoreArrayItem( mData[2] );
+            }
+
+            void TestEqual( Vec3 &c2 )
+            {
+                for ( uint32_t i = 0; i < 3; ++i )
+                {
+                    EXPECT_FLOAT_EQ( mData[i], c2.mData[i] );
+                }
+            }
+
+        private:
+
+            float mData[3];
+        };
+
+        struct SubMeshData
+        {
+            SubMeshData() : indexOffset( 0 ), indexCount( 1 ), vertexOffset( 2 ), materialIndex( 3 ),
+                aabbMin( 0.0, 0.1, 0.2 ), aabbMax( 0.3, 0.4, 0.5 ), aabbOffset( 0.6, 0.7, 0.8 ),
+                name( "MAGIC_STRING_1" )
+            {
+
+            }
+
+            SubMeshData( Vec3 &v1, Vec3 &v2, Vec3 &v3 ) : indexOffset( 0 ), indexCount( 1 ), vertexOffset( 2 ), materialIndex( 3 ),
+                aabbMin( v1 ), aabbMax( v2 ), aabbOffset( v3 ), name( "MAGIC_STRING_2" )
+            {
+
+            }
+
+            uint32_t indexOffset;
+            uint32_t indexCount;
+            uint32_t vertexOffset;
+            uint32_t materialIndex;
+
+            Vec3 aabbMin;
+            Vec3 aabbMax;
+            Vec3 aabbOffset;
+
+            std::string name;
+
+            void OnStore( Message &message )
+            {
+                message.Store( indexOffset, 0 );
+                message.Store( indexCount, 1 );
+                message.Store( vertexOffset, 2 );
+                message.Store( materialIndex, 3 );
+
+                message.Store( aabbMin, 4 );
+                message.Store( aabbMax, 5 );
+                message.Store( aabbOffset, 6 );
+
+                message.Store( name, 7 );
+            }
+
+            void TestEqual( SubMeshData &c2 )
+            {
+                EXPECT_EQ( indexOffset, c2.indexOffset );
+                EXPECT_EQ( indexCount, c2.indexCount );
+                EXPECT_EQ( vertexOffset, c2.vertexOffset );
+                EXPECT_EQ( materialIndex, c2.materialIndex );
+                EXPECT_EQ( name, c2.name );
+
+                aabbMin.TestEqual( c2.aabbMin );
+                aabbMax.TestEqual( c2.aabbMax );
+                aabbOffset.TestEqual( c2.aabbOffset );
+            }
+        };
+
+        ZefirosEngineMeshData( uint32_t seed = 233232 )
+        {
+            srand( seed );
+
+            mSubMeshes.reserve( its1 );
+            mPositionVertices.reserve( its2 );
+
+
+            for ( uint32_t i = 0; i < its1; ++i )
+            {
+                Vec3 v1( GetRandom< float > (), GetRandom< float > (), GetRandom< float > () );
+                Vec3 v2( GetRandom< float > (), GetRandom< float > (), GetRandom< float > () );
+                Vec3 v3( GetRandom< float > (), GetRandom< float > (), GetRandom< float > () );
+
+                mSubMeshes.push_back( SubMeshData( v1, v2, v3 ) );
+            }
+
+            for ( uint32_t i = 0; i < its2; ++i )
+            {
+                Vec3 v4( GetRandom< float > (), GetRandom< float > (), GetRandom< float > () );
+                mPositionVertices.push_back( v4 );
+            }
+
+            mInterlacedVertices.resize( its3 );
+            mIndices.resize( its4 );
+        }
+
+        void SERIALISATION_CUSTOM_INTERFACE( Message &message )
+        {
+            message.StoreContainer( mSubMeshes, 0 );
+            message.StoreContainer( mPositionVertices, 1 );
+            message.StoreContainer( mInterlacedVertices, 2 );
+            message.StoreContainer( mIndices, 3 );
+        }
+
+        void TestEqual( ZefirosEngineMeshData &c2 )
+        {
+            for ( uint32_t i = 0; i < its1; ++i )
+            {
+                mSubMeshes[i].TestEqual( c2.mSubMeshes[i] );
+            }
+
+            for ( uint32_t i = 0; i < its2; ++i )
+            {
+                mPositionVertices[i].TestEqual( c2.mPositionVertices[i] );
+            }
+
+            for ( uint32_t i = 0; i < its3; ++i )
+            {
+                EXPECT_EQ( mInterlacedVertices[i], c2.mInterlacedVertices[i] );
+            }
+
+            for ( uint32_t i = 0; i < its4; ++i )
+            {
+                EXPECT_EQ( mIndices[i], c2.mIndices[i] );
+            }
+        }
+
+        std::vector< SubMeshData > mSubMeshes;
+        std::vector< Vec3 > mPositionVertices;
+        std::vector< uint8_t > mInterlacedVertices;
+        std::vector< uint8_t > mIndices;
+    };
+
     template< uint32_t its = 100 >
     class TestClass3
         : public ISerialisable
@@ -416,4 +587,6 @@ namespace TestClasses
 
     TestRepeatedPrimitiveMessageClass( RepeatedPrimitiveMessage, randomVals, 343422, 21331, 100, float );
     TestRepeatedPrimitiveMessageClass( RepeatedPrimitiveMessage, randomVals, 343422, 21331, 100, double );
+
+    TestMeshDataClass( ZefirosMeshData, randomVals, 343422, 21331, 382, 533055, 29851080, 1573602 );
 }
