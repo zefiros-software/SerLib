13fbe700163163598fe06d58ab6890d32460ff8e
Paul Visscher <paul.pev@hotmail.com>
2015-08-28 19:12:08 +0200

    Coverage

---
+++ generateCoverage.sh
@@ -0,0 +1,8 @@
+rm -rf gcov
+rm -rf target/site/clover
+mkdir gcov
+cd gcov/
+gcov --object-directory ../bin/obj/x32/Coverage/serialisation-test/* ../test
+lcov --capture --base-directory ../test --directory .. --output-file coverage.info
+genhtml coverage.info --output-directory ../target/site/clover
+python  ../extern/gcov/gcov_to_clover.py *.gcov
