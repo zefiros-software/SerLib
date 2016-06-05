d59e1067d3a82b6e87fd3a35c66cd61ca73756b5
PaulVisscher <paul.pev@hotmail.com>
2016-06-05 16:11:36 +0200

    Improved directories

--- generateCoverage.sh
+++ generateCoverage.sh
@@ -1,8 +1,7 @@
 rm -rf gcov
 rm -rf target/site/clover
 mkdir gcov
-cd gcov/
-gcov --object-directory ../bin/obj/x86/Coverage/serialisation-test/* ../test
-lcov --capture --base-directory ../test --directory .. --directory ../test/ --directory ../serialisation/include/ --output-file coverage.info --no-external
-genhtml coverage.info --output-directory ../target/site/clover
-python  ../assets/Zefiros-Software/SerLib/Zefiros-Software/gcov2clover/gcov_to_clover.py *.gcov
+gcov --object-directory bin/obj/x86/Coverage/serialisation-test/* test
+lcov --capture --base-directory test --directory . --directory test/ --directory serialisation/include/ --output-file gcov/coverage.info --no-external
+genhtml gcov/coverage.info --output-directory target/site/clover
+python  assets/Zefiros-Software/SerLib/Zefiros-Software/gcov2clover/gcov_to_clover.py gcov/*.gcov
