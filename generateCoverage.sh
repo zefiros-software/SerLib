f2a8cb9b03b5109aae85412bf229312d5246283f
PaulVisscher <paul.pev@hotmail.com>
2016-06-05 16:25:48 +0200

    Fixed coverage

--- generateCoverage.sh
+++ generateCoverage.sh
@@ -1,7 +1,10 @@
 rm -rf gcov
 rm -rf target/site/clover
 mkdir gcov
+cd cgov
 gcov --object-directory bin/obj/x86/Coverage/serialisation-test/* test
-lcov --capture --base-directory test --directory . --directory test/ --directory serialisation/include/ --output-file gcov/coverage.info --no-external
+cd ../
+lcov --capture --base-directory test --directory ./ --directory test/ --directory serialisation/include/ --output-file gcov/coverage.info --no-external
 genhtml gcov/coverage.info --output-directory target/site/clover
-python  assets/Zefiros-Software/SerLib/Zefiros-Software/gcov2clover/gcov_to_clover.py gcov/*.gcov
+pwd
+python assets/Zefiros-Software/SerLib/Zefiros-Software/gcov2clover/gcov_to_clover.py gcov/*.gcov
