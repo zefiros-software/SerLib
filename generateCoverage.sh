d51d260a95c87cedf2299375d4d99a2649372e9b
PaulVisscher <paul.pev@hotmail.com>
2016-06-05 13:51:41 +0200

    Fixed inclusion

--- generateCoverage.sh
+++ generateCoverage.sh
@@ -5,4 +5,4 @@ cd gcov/
 gcov --object-directory ../bin/obj/x86/Coverage/serialisation-test/* ../test
 lcov --capture --base-directory ../test --directory .. --directory ../test/ --directory ../serialisation/include/ --output-file coverage.info --no-external
 genhtml coverage.info --output-directory ../target/site/clover
-python  ../assets/Zefiros-Software/SerLib-Dev/Zefiros-Software/gcov2clover/gcov_to_clover.py *.gcov
+python  ../assets/Zefiros-Software/SerLib/Zefiros-Software/gcov2clover/gcov_to_clover.py *.gcov
