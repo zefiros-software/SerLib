9ea8603e8c6cf987a44c568727a88de2fa2e880c
PaulVisscher <paul.pev@hotmail.com>
2016-08-14 12:01:58 +0200

    Appveyor added

---
+++ script/appveyor-buid.bat
@@ -0,0 +1,20 @@
+premake5 install-package --allow-install --allow-module  || exit /b
+premake5 vs2015 || exit /b
+msbuild bsp/SerLib.sln /property:Configuration=Release /property:Platform=Win32 || exit /b
+::msbuild bsp/SerLib.sln /property:Configuration=Debug /property:Platform=Win32 || exit /b
+msbuild bsp/SerLib.sln /property:Configuration=Release /property:Platform=x64 || exit /b
+::msbuild bsp/SerLib.sln /property:Configuration=Debug /property:Platform=x64 || exit /b
+
+cd test/
+premake5 vs2015 || exit /b
+msbuild zpm/SerLib-ZPM.sln || exit /b
+
+cd ../
+
+bin\x86\serialisation-test.exe || exit /b
+::bin\x86\serialisation-testd.exe || exit /b
+
+bin\x86_64\serialisation-test.exe || exit /b
+::bin\x86_64\serialisation-testd.exe || exit /b
+
+test\bin\x86\serialisation-zpm-test.exe || exit /b
