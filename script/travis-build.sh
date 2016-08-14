55a1f2c7c3a082afcb1ff7c195978a26ce4f3645
PaulVisscher <paul.pev@hotmail.com>
2016-08-14 10:50:17 +0200

    Travis build enabled

---
+++ script/travis-build.sh
@@ -0,0 +1,29 @@
+set -e
+
+premake5 install-package --allow-install --allow-module
+premake5 gmake
+cd serialisation
+make config=debug_x86
+make config=debug_x86_64
+make config=release_x86
+make config=release_x86_64
+#make config=coverage_x86
+#make config=coverage_x86_64
+
+cd ../test/
+
+premake5 gmake
+
+cd zpm/
+make
+cd ../
+
+../../bin/x86/serialisation-test
+../../bin/x86/serialisation-testd
+#../../bin/x86/serialisation-testcd
+
+../../bin/x86_64/serialisation-test
+../../bin/x86_64/serialisation-testd
+#../../bin/x86_64/serialisation-testcd
+
+../../test/bin/x86/serialisation-zpm-test
