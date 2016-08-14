5f5d8569a9ee4bdd9b8b3b548f36b318a64ed3f1
PaulVisscher <paul.pev@hotmail.com>
2016-08-15 01:31:17 +0200

    Set error code

--- script/appveyor-build.bat
+++ script/appveyor-build.bat
@@ -1,21 +1,21 @@
-premake5 install-package --allow-install --allow-module  || exit /b
-premake5 vs2015 || exit /b
-msbuild serialisation/SerLib.sln /property:Configuration=Release /property:Platform=Win32 || exit /b
-::msbuild serialisation/SerLib.sln /property:Configuration=Debug /property:Platform=Win32 || exit /b
-msbuild serialisation/SerLib.sln /property:Configuration=Release /property:Platform=x64 || exit /b
-::msbuild serialisation/SerLib.sln /property:Configuration=Debug /property:Platform=x64 || exit /b
+premake5 install-package --allow-install --allow-module  || exit /b 1
+premake5 vs2015 || exit /b 1
+msbuild serialisation/SerLib.sln /property:Configuration=Release /property:Platform=Win32 || exit /b 1
+::msbuild serialisation/SerLib.sln /property:Configuration=Debug /property:Platform=Win32 || exit /b 1
+msbuild serialisation/SerLib.sln /property:Configuration=Release /property:Platform=x64 || exit /b 1
+::msbuild serialisation/SerLib.sln /property:Configuration=Debug /property:Platform=x64 || exit /b 1
 
 cd test/
-premake5 vs2015 || exit /b
-msbuild zpm/SerLib-ZPM.sln || exit /b
+premake5 vs2015 || exit /b 1
+msbuild zpm/SerLib-ZPM.sln || exit /b 1
 
 cd ..\bin\x86\
-.\serialisation-test.exe || exit /b
-::.\serialisation-testd.exe || exit /b
+.\serialisation-test.exe || exit /b 1
+::.\serialisation-testd.exe || exit /b 1
 
 cd ..\x86_64\
-.\serialisation-test.exe || exit /b
-::.\serialisation-testd.exe || exit /b
+.\serialisation-test.exe || exit /b 1
+::.\serialisation-testd.exe || exit /b 1
 
 cd ..\..\test\bin\
-.\x86\serialisation-zpm-test.exe || exit /b
+.\x86\serialisation-zpm-test.exe || exit /b 1
