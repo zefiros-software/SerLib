0185654aea621a1fff86aa531e68b51a03ab72f0
Paul Visscher <paul.pev@hotmail.com>
2016-08-23 21:54:25 +0200

    Revert "Set root"
    This reverts commit 22225f803cca85eea097eeb9d916c20a1a3155a3.

--- script/coverage.sh
+++ script/coverage.sh
@@ -4,4 +4,4 @@ cd serialisation
 make config=coverage_x86
 cd ../
 
-coveralls --root bin/obj/x86/Coverage/ --exclude test --exclude extern --gcov-options '\-lp'
+coveralls --build-root bin/obj/x86/Coverage/ --exclude test --exclude extern --gcov-options '\-lp'
