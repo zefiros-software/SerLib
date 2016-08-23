22225f803cca85eea097eeb9d916c20a1a3155a3
Paul Visscher <paul.pev@hotmail.com>
2016-08-23 18:55:09 +0200

    Set root

--- script/coverage.sh
+++ script/coverage.sh
@@ -4,4 +4,4 @@ cd serialisation
 make config=coverage_x86
 cd ../
 
-coveralls --build-root bin/obj/x86/Coverage/ --exclude test --exclude extern --gcov-options '\-lp'
+coveralls --root bin/obj/x86/Coverage/ --exclude test --exclude extern --gcov-options '\-lp'
