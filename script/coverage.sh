09c7293c33a12b2a7a06ceaa3977e5353e80f823
Paul Visscher <paul.pev@hotmail.com>
2016-08-25 14:01:17 +0200

    Fix coverage report

--- script/coverage.sh
+++ script/coverage.sh
@@ -2,10 +2,8 @@ set -e
 
 cd serialisation
 make config=coverage_x86
-cd ../bin/x86/
+cd ../
 
 ./serialisation-testcd
 
-cd ../../
-
-codecov --gcov-root bin/obj/x86/Coverage/serialisation-test/
+codecov --gcov-root serialisation
